2575a29e1f30f9da5fcc532168596db221d096c1
falun <richard@turbinelabs.io>
2016-11-11 15:51:42 -0800

    Organization around docker files
    This moves the Dockerfile and ENV files into directories based on the container
    they're describing.
    
    Additionally a minor config tweak to make the bundled PHP specify a good
    default time zone (UTC).

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+ROOTDIR=${GPINGIO_HOME:-.}
+
 echo Building DB image
-docker build -t gping.db:latest -f docker/db.Dockerfile .
+docker build -t gping.db:latest -f docker/db/Dockerfile ${ROOTDIR}
 
 echo
 echo
 echo Building WWW image
-docker build -t gping.io:latest -f docker/www.Dockerfile .
+docker build -t gping.io:latest -f docker/www/Dockerfile ${ROOTDIR}
