2b154bb51034f0b6b06e40780ab4c8cb92cbe173
falun <richard@turbinelabs.io>
2016-11-11 18:57:02 -0800

    www container now defaults to using volume mount
    - run dos2unix on shell scripts
    - convert www container to treat webapp as a directory mounted from host
    - introduce new container for live code

--- docker/build.sh
+++ docker/build.sh
@@ -12,4 +12,4 @@ docker build -t gping.db:latest -f docker/db/Dockerfile .
 echo
 echo
 echo Building WWW image
-docker build -t gping.io:latest -f docker/www/Dockerfile .
+docker build -t gping.io:live -f docker/www/live.Dockerfile .
--- docker/www.sh
+++ docker/www.sh
@@ -11,10 +11,11 @@ fi
 cd ${GPINGIO_HOME}
 docker kill ${WWW_CONTAINER}
 docker rm ${WWW_CONTAINER}
-docker run                  \
-  -p 8080:80                \
-  --env-file docker/www/env \
-  --name ${WWW_CONTAINER}   \
-  --link ${DB_CONTAINER}    \
-  -d                        \
-  gping.io:latest
+docker run                             \
+  -p 8080:80                           \
+  --env-file docker/www/env            \
+  --name ${WWW_CONTAINER}              \
+  --link ${DB_CONTAINER}               \
+  -d                                   \
+  -v ${GPINGIO_HOME}/www:/var/www/html \
+  gping.io:live
