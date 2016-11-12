60185ac9999c0022a3dc89335e21fd24f9cc88c6
falun <richard@turbinelabs.io>
2016-11-11 21:54:08 -0800

    Prettier docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -6,10 +6,32 @@ if [ -z $GPINGIO_HOME ]; then
 fi
 
 cd $GPINGIO_HOME
-echo Building DB image
-docker build -t gping.db:latest -f docker/db/Dockerfile .
+mkdir -p .logs
 
-echo
-echo
-echo Building WWW image
-docker build -t gping.io:live -f docker/www/live.Dockerfile .
+echo Building
+echo "  - gping.db:latest"
+docker build -t gping.db:latest -f docker/db/Dockerfile .  > .logs/docker-db.log 2>&1
+if [ ! $? -eq 0 ]; then
+  cat .logs/docker-db.log
+  echo
+  echo container build failed
+  exit 1
+fi
+
+echo "  - gping.io:live"
+docker build -t gping.io:live -f docker/www/live.Dockerfile . > .logs/dacker-www-live.log 2>&1
+if [ ! $? -eq 0 ]; then
+  cat .logs/docker-www-live.log
+  echo
+  echo container build failed
+  exit 1
+fi
+
+echo "  - gping.io:latest"
+docker build -t gping.io:latest -f docker/www/Dockerfile .  > .logs/docker-www-latest.log 2>&1
+if [ ! $? -eq 0 ]; then
+  cat .logs/docker-www-live.log
+  echo
+  echo container build failed
+  exit 1
+fi
