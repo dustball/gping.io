c5822bebe87e6de0d8216beb34d4487776d7870d
Brian Klug <brian.klug@gmail.com>
2017-01-02 16:07:21 -0800

    More unit tests around ping & ping API

--- test/ping_test.php
+++ test/ping_test.php
@@ -17,5 +17,11 @@ class ping_test extends PHPUnit_Framework_TestCase {
     $this->assertTrue($response->isSuccess());
   } 
 
-      
+  public function test_ping_api() {
+    $json = file_get_contents("http://localhost:80/ping?ver=1");
+    $o = json_decode($json);
+    $this->assertEquals($o->result,"ok");
+    $this->assertEquals(strlen($o->id),12);
+  } 
+        
 }
